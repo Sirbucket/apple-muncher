22e9a6460a36f994cd1706340d371ff0b289796b
William Tuleja <51178809+Sirbucket@users.noreply.github.com>
2022-04-26 16:19:14 +0000

    Huge balance change.

--- app/app.ts
+++ app/app.ts
@@ -1,46 +1,23 @@
 const image: HTMLDivElement = document.querySelector(".image");
 const score: HTMLDivElement = document.querySelector('.score');
-const ms: number = 1000;
-
+const ms: number = 500;
+const numMod: number = 200;
+const multMod: number = 1.25;
 let clicks: number = 0;
-let nummod: number = 100;
-let clickmod: number = 1;
+let clickMod: number = 1;
 let currentClickMod: number = 1;
 
-const clickNums = [
-	2,
-	8,
-	64,
-	256
-]
-
-const clickNames = [
-	"+2 Clicks (40)",
-	"+8 Clicks (160)",
-	"+64 Clicks (1280)",
-	"+256 Clicks (5120)"
-]
-
-const buttonNums = [
-	1,
-	5,
-	10,
-	100
-];
-
-const buttonNames = [
-	"+1/s (Cost: 100)",
-	"+5/s (Cost: 500)",
-	"+10/s (Cost: 1000)",
-	"+100/s (Cost: 10000)"
-];
+const clickNums = [1,2,5,10];
+const buttonNums = [1,5,10,25];
+const clickNames = ["+1 Clicks","+2 Clicks","+5 Clicks","+10 Clicks"];
+const buttonNames = ["+2/s","+10/s","+20/s","+50/s"];
 
 //Add 1 to clicks, convert clicks to a string as score's text content.
 const updateScore = (count: number) => {
 	for (let i = 0; i < count; ++i) {
 		++clicks;
 	}
-	score.textContent = clicks.toString();
+	score.textContent = Math.round(clicks).toString();
 }
 
 //Marks button
@@ -56,21 +33,51 @@ const removeMark = (button: HTMLButtonElement, str: string) => {
 //Makes a button
 const makeButton = (number: number, str: string) => {
 	const button: HTMLButtonElement = document.createElement('button');
+	let cost: number = Math.round(number * numMod);
 	button.addEventListener("click", () => {
-		if (clicks >= number * nummod) {
+		if (clicks >= cost) {
 			removeMark(button, "no");
 			markButton(button, "yes");
 			setInterval(updateScore, ms, number);
-			clicks -= number * nummod;
-			score.textContent = clicks.toString();
+
+			clicks -= Math.round(number * numMod);
+
+			const newCost = Math.round(cost *= multMod);
+			button.textContent = `${str} (Cost: ${newCost.toString()})`;
+			score.textContent = Math.round(clicks).toString();
 		}
 		else {
 			removeMark(button, "yes");
 			markButton(button, "no");
 		}
 	});
-	button.textContent = str;
+	button.textContent = `${str} (Cost: ${cost.toString()})`;
 	return button;
+}
+
+//Makes a button for adding clicks when you click.
+const makeClickerButton = (number: number, str: string) => {
+	const clickerButton: HTMLButtonElement = document.createElement('button');
+	let cost = number * (numMod * 3);
+	clickerButton.addEventListener("click", () => {
+		if (clicks >= cost) {
+			removeMark(clickerButton, "no");
+			markButton(clickerButton, "yes");
+			
+			currentClickMod += Math.round(clickMod * number);
+			clicks -= cost;
+			
+			const newCost = Math.round(cost *= multMod);
+			clickerButton.textContent = `${str} (Cost: ${newCost.toString()})`;
+			score.textContent = Math.round(clicks).toString();
+		}
+		else {
+			removeMark(clickerButton, "yes");
+			markButton(clickerButton, "no");
+		}
+	});
+	clickerButton.textContent = `${str} (Cost: ${cost.toString()})`;
+	return clickerButton;
 }
 
 //Batches buttons
@@ -80,26 +87,6 @@ const addButtons = (button: HTMLButtonElement) => {
 			button.appendChild(makeButton(buttonNums[t], buttonNames[t]));
 		}
 	}
-}
-
-//Makes a button for adding clicks when you click.
-const makeClickerButton = (number: number, str: string) => {
-	const clickerButton: HTMLButtonElement = document.createElement('button');
-	clickerButton.addEventListener("click", () => {
-		if (clicks >= number * (nummod / 5)) {
-			removeMark(clickerButton, "no");
-			markButton(clickerButton, "yes");
-			currentClickMod += clickmod * number;
-			clicks -= number * (nummod / 5);
-			score.textContent = clicks.toString();
-		}
-		else {
-			removeMark(clickerButton, "yes");
-			markButton(clickerButton, "no");
-		}
-	});
-	clickerButton.textContent = str;
-	return clickerButton;
 }
 
 //Batches buttons
@@ -114,7 +101,7 @@ const addClickerButtons = (clickerButton: HTMLButtonElement) => {
 //Runs buttons
 const runButtons = () => {
 	addButtons(document.querySelector(".buttons"));
-	addClickerButtons(document.querySelector(".clicker-buttons"))
+	addClickerButtons(document.querySelector(".clicker-buttons"));
 }
 
 //Runs game logic
